4bb9e48dd02a6fbc7bc867d1c1e60799362f2cf7
Masnoon Junaid <masnoon20@gmail.com>
2019-03-23 14:00:50 +0530

    cat clicker premium work

--- js/app.js
+++ js/app.js
@@ -5,3 +5,37 @@ games.addEventListener('click', function(){
   count++;
   text.innerHTML = count;
 });
+ let cats = {
+   models : [
+     {
+       name : 'Anne',
+       url : 'images/anne.jpg',
+       clicks: 0
+     },
+     {
+       name: 'Emilia',
+       url: 'images/emilia.jpg',
+       clicks: 0
+     },
+     {
+       name : 'Janifer',
+       url : 'images/jenifer.jpg',
+       clicks:0
+     },
+     {
+       name : 'Scarlet',
+       url:'images/carlet.jpg',
+       clicks: 0
+     },
+     {
+       name : 'Selena',
+       url: 'images/selena.jpg',
+       clicks:0
+     }
+   ]
+ };
+ let division = document.getElementById('insert');
+for (let i = 0; i < cats.models.length; i++) {
+  const element = document.createElement('li');
+    let list = division.insertAdjacentHTML('beforeend', element);
+}
